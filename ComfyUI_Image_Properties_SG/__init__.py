4d14ce3f763ce09967c2b3e6bdf43ca77bec0258
Shammi G <shammig.sng@gmail.com>
2025-10-25 21:17:01 +0530

    Add files via upload

---
+++ ComfyUI_Image_Properties_SG/__init__.py
@@ -0,0 +1,23 @@
+"""
+Loads image with drag-and-drop and automatically display image properties
+"""
+
+from .Load_Image_and_view_Properties_SG import LoadImageandviewPropertiesSG
+from .View_Image_Properties_SG import ViewImagePropertiesSG
+from .Preview_Image_and_view_Properties_SG import PreviewImageandviewPropertiesSG
+
+NODE_CLASS_MAPPINGS = {
+    "ViewImagePropertiesSG": ViewImagePropertiesSG,
+    "LoadImageandviewPropertiesSG": LoadImageandviewPropertiesSG,
+    "PreviewImageandviewPropertiesSG": PreviewImageandviewPropertiesSG
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ViewImagePropertiesSG": "View Image Properties-SG",
+    "LoadImageandviewPropertiesSG": "Load Image and view Properties-SG",
+    "PreviewImageandviewPropertiesSG": "Preview Image and view Properties-SG"
+}
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
